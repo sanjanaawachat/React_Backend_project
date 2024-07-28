e83b649520bcc4987a6d8f0e563e0577d5eefe5a
sanjana awachat <you@example.com>
2024-07-28 10:40:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+ const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/productdb',
+   {useNewUrlParser: true,useUnifiedTopology: true});
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  image: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+app.post('/api/products', async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.send(product);
+});
+
+app.get('/api/products', async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
